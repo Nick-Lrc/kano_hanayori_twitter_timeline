587528d44369a0e9c04f73a50610eb485c6de274
Nick Liu <geminid@protonmail.com>
2021-05-31 00:50:37 -0400

    Discard mapping from twitter id to media lists

--- media_mapper.py
+++ media_mapper.py
@@ -23,7 +23,6 @@ def export_data(data, dst):
 if __name__ == '__main__':
     log = import_data('log.pickle')
     url_to_name = {}
-    tid_to_media = defaultdict(dict)
     for tid, tweet in sorted(log.items()):
         urls = get_urls(tweet)
         for idx, url in urls:
@@ -32,7 +31,5 @@ if __name__ == '__main__':
                 name = url_to_name[url]
             else:
                 url_to_name[url] = name
-            tid_to_media[tid][idx] = name
     print(f'Found {len(url_to_name)} unique urls.')
     export_data(url_to_name, 'url_to_name.json')
-    export_data(tid_to_media, 'tid_to_media.json')
